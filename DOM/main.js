808ca59ae775a1113a4aeaebc0bff4f4f3596a95
Chimobim Enebe <chimobimenebe@gmail.com>
2024-08-14 15:18:30 +0100

    Committed

--- DOM/main.js
+++ DOM/main.js
@@ -28,3 +28,40 @@ console.log(document.body.lastChild); //Returns Last Node
 // FirstElementChild and LastElement Child
 console.log(document.body.firstElementChild); //Returns First Element
 console.log(document.body.lastElementChild); //Returns Last Element
+
+console.log(document.body.children[2].lastElementChild);
+
+// Searching DOM
+// .getElementByTagName()
+// .getElementByClassName()
+// .getElementById()
+// .querySelector()
+// .querySelectorAll()
+
+// .getElementByTagName() - Returns the HTML Collection of all the Elements searched from the document
+const liTags = document.getElementsByTagName("li");
+console.log(liTags);
+
+console.log(liTags[4]);
+console.log(liTags[0]);
+
+// .getElementByClassName - Returns the HTML Collection of all the Elements that match the class
+const classElements = document.getElementsByClassName("DOM");
+console.log(classElements[2]);
+
+// .getElementsByID - returns a single element that matches the search criteria
+const idElement = document.getElementById("h2");
+console.log(idElement);
+
+const fig = document.getElementById("fig");
+console.log(fig);
+
+// querySelector
+const elem = document.querySelector("section p");
+console.log(elem);
+
+// querySelectorAll
+const elem2 = document.querySelectorAll("h2, li, DOM, p");
+console.log(elem2);
+
+// Manipulating or Modifying the DOM
