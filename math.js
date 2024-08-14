7c0acb3da430693e276ea7231b6f9c12d4f36d7c
Chimobim Enebe <chimobimenebe@gmail.com>
2024-08-14 13:16:12 +0100

    Committed

--- math.js
+++ math.js
@@ -1,17 +1,15 @@
 // const arr = new Array(4, 6, 7);
 
-
 console.log(3 ** 2);
-
 
 // Math.pow this takes item to the power of 2
 
 console.log(Math.pow(3, 2));
 
-
-Math.abs(-392)
+Math.abs(-392);
 // absolute value of a number
 
 console.log(Math.PI);
 console.log(Math.max);
 console.log(Math.min);
+
