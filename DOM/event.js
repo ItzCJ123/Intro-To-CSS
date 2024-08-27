8b6eacb2747a02157fb59071ab48449eb464823e
Chimobim Enebe <chimobimenebe@gmail.com>
2024-08-27 13:47:19 +0100

    "Updated event.js and page2.html files with new functionality and styles for login form, error handling, and search input."

--- DOM/event.js
+++ DOM/event.js
@@ -45,10 +45,60 @@ function handleSubmit(e) {
   if (!password.value) {
     password.style.border = "1px solid red";
   }
-  console.log(username.value);
-  console.log(password.value);
+
+  // console.log(username.value);
+  // console.log(password.value);
+
+  const user = db.filter((item) => item.username === username.value);
+  if (!user.length) {
+    document.querySelector(".err").innerHTML = "Username does not exist!"
+    document.querySelector(".err").style.display = "block"
+    // alert("User does not exist!");
+    return;
+  }
+  if (user[0].password !== password.value) {
+      document.querySelector(".err").innerHTML = "Invalid Username or passowrd"
+    document.querySelector(".err").style.display = "block"
+    return;
+  }
+  document.querySelector(".login").style.display = "block"
+  document.getElementById("form").style.display = "none"
+  // alert("Sign In Successful!")
+
 }
 
 document.getElementById("form").onsubmit = (e) => {
   handleSubmit(e);
 };
+
+const arr = [5, 6, 8, 1, 134, 1, 45, 86, 20, 87, 36];
+const filtered = arr.filter((x) => x % 2 !== 0);
+console.log(filtered);
+
+// Removing error Messages
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+username.onfocus = () => {
+  username.style.borderColor = "#000";
+  document.querySelector(".err").style.display = "none";
+}
+password.onfocus = () => {
+  username.style.borderColor = "#000";
+  document.querySelector(".err").style.display = "none";
+}
+
+document.querySelector("span").addEventListener("click", ()=>{
+  if (password.type === "password") {
+    password.type = "text"
+  } else{
+    password.type = "password"
+  }
+})
+
+const search = document.querySelector("#search")
+const dis = document.querySelector(".display")
+search.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  dis.innerHTML = search.value
+})
