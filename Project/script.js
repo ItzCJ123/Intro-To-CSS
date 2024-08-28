6326c780a029864abe2fc74f199daaf6737ef3d7
Chimobim Enebe <chimobimenebe@gmail.com>
2024-08-28 13:30:36 +0100

    Committed

--- Project/script.js
+++ Project/script.js
@@ -12,6 +12,25 @@ const getMovies = async () => {
     const response = await fetch(url, options);
     const result = await response.json();
     console.log(result);
+    const container =document.getElementsByClassName("container")[0]
+    result.forEach((movie) =>{
+        const div = document.createElement('div')
+        div.className = "movie-card"
+        div.innerHTML = `<figure>
+                        <img src=${movie.image} alt="">
+                    </figure>
+                    <div class="title-sec">
+                        <h3 class="title">${movie.title}</h3>
+                        <p class="year">${movie.year}</p>
+                    </div>
+                    <p class="desc"><span>Sypnosis:</span>${movie.description}</p>
+                    <div class="genre-sec">
+                        <p class="genre">${movie.genre.join(', ')}</p>
+                        <p class="rating"> <i class="fa-solid fa-star"></i> ${movie.rating}</p>
+                    </div>`;
+
+                    container.prepend(div)
+    })
   } catch (error) {
     console.error(error);
   }
